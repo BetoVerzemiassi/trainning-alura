815638a8244f697814b6e0b3fee702e48f726720
alberto.borguesani <alberto.borguesani@infracommerce.com.br>
2017-07-05 17:40:17 -0300

    Aula01 Introdução Javascript

---
+++ JAVASCRIPT-PROGRAMANDO-NA-LINGUAGEM-WEB/introducao-javascript/js/princiál.js
@@ -0,0 +1,9 @@
+
+			// alert("Olá Mundo");
+			// console.log("Olá mundo");
+			// console.log(document);
+			// console.log(document.querySelector("h1"));
+			var titulo = document.querySelector(".titulo");
+			console.log(titulo.textContent);
+			titulo.textContent = "Aparecida Nutricionista";
+			
