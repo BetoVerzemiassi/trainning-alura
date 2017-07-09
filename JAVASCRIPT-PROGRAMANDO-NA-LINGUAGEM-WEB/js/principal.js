9cad2ad4d58b36dd6b71b2c8c5807f05cc2e5168
BetoVerzemiassi <betoverzemiassi@gmail.com>
2017-07-09 17:56:44 -0300

    Aula 03 Array, Loop e Estilos

--- JAVASCRIPT-PROGRAMANDO-NA-LINGUAGEM-WEB/js/principal.js
+++ JAVASCRIPT-PROGRAMANDO-NA-LINGUAGEM-WEB/js/principal.js
@@ -12,41 +12,95 @@
 
 			var paciente = document.querySelector("#primeiro-paciente");
 
-			var tdPeso = paciente.querySelector(".info-peso");
-			var tdAltura = paciente.querySelector(".info-altura");
+			// var tdPeso = paciente.querySelector(".info-peso");
+			// var tdAltura = paciente.querySelector(".info-altura");
 
-			var peso = tdPeso.textContent;
-			var altura = tdAltura.textContent;
+			// var peso = tdPeso.textContent;
+			// var altura = tdAltura.textContent;
 
-			var imc = peso / (altura * altura);
-			var tdImc = paciente.querySelector(".info-imc");
-			tdImc.textContent = imc;
+			// var imc = peso / (altura * altura);
+			// var tdImc = paciente.querySelector(".info-imc");
+			// tdImc.textContent = imc;
 
-			// console.log(imc);
+			// // console.log(imc);
 
-			var tdIMC = paciente.querySelector(".info-imc");
+			// var tdIMC = paciente.querySelector(".info-imc");
 
-			var pesoEhValido = true;
-			var alturaEhValida = true;
+			// var pesoEhValido = true;
+			// var alturaEhValida = true;
 
-			if(peso <= 0 || peso >= 1000){
-				// console.log("Peso inválido!");
-				pesoEhValido = false;
-				tdImc.textContent = "Peso inválido!";
-			}	
+			// if(peso <= 0 || peso >= 1000){
+			// 	// console.log("Peso inválido!");
+			// 	pesoEhValido = false;
+			// 	tdImc.textContent = "Peso inválido!";
+			// }	
 
-			if (altura <= 0 || altura >= 3.00) {
-			    // console.log("Altura inválida!");
-			    alturaEhValida = false;
-			    tdImc.textContent = "Altura inválida!";
-			}
+			// if (altura <= 0 || altura >= 3.00) {
+			//     // console.log("Altura inválida!");
+			//     alturaEhValida = false;
+			//     tdImc.textContent = "Altura inválida!";
+			// }
 
-			if(alturaEhValida && pesoEhValido){
-				var imc = peso / (altura * altura);
-				tdIMC.textContent = imc;
-			}			
+			// if(alturaEhValida && pesoEhValido){
+			// 	var imc = peso / (altura * altura);
+			// 	tdIMC.textContent = imc;
+			// }			
 			
 			// console.log(paciente);
 			// console.log(tdPeso);
 			// console.log(peso);
 			// console.log(altura);
+
+			/*Aula 03 Arrays, Loop e Estilos*/
+
+			//querySelectorAll = busca todos os os elementos com a classe ou id
+			//toFixed() = limita as casas decimais, entre parentese recebe o valor
+			//style - manipula o estilo do elemento
+			//backgroundColor - Modifica a cor de fundo do elemento HTML
+			//classList.add - adicionando estilo ao elemento HTML
+
+			var pacientes = document.querySelectorAll(".paciente");
+
+			for(var i = 0; i < pacientes.length; i++){
+				// console.log(pacientes[i]);
+
+				var paciente = pacientes[i];
+
+				var tdPeso = paciente.querySelector(".info-peso");
+				var tdAltura = paciente.querySelector(".info-altura");
+
+				var peso = tdPeso.textContent;
+				var altura = tdAltura.textContent;
+
+				var imc = peso / (altura * altura);
+				var tdImc = paciente.querySelector(".info-imc");
+				tdImc.textContent = imc;
+
+				// console.log(imc);
+
+				var tdIMC = paciente.querySelector(".info-imc");
+
+				var pesoEhValido = true;
+				var alturaEhValida = true;
+
+				if(peso <= 0 || peso >= 1000){
+					// console.log("Peso inválido!");
+					pesoEhValido = false;
+					tdImc.textContent = "Peso inválido!";
+					// paciente.style.backgroundColor = "lightcoral";
+					paciente.classList.add("paciente-invalido");
+				}	
+
+				if (altura <= 0 || altura >= 3.00) {
+				    // console.log("Altura inválida!");
+				    alturaEhValida = false;
+				    tdImc.textContent = "Altura inválida!";
+				    // paciente.style.backgroundColor = "lightcoral";
+				    paciente.classList.add("paciente-invalido");
+				}
+
+				if(alturaEhValida && pesoEhValido){
+					var imc = peso / (altura * altura);
+					tdIMC.textContent = imc.toFixed(2);
+				}		
+			}
