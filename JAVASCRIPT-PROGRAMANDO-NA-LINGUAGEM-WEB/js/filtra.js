22d62a223a43b22eb198ebe730d1c3efac5dd040
alberto.borguesani <alberto.borguesani@infracommerce.com.br>
2017-07-21 13:56:45 -0300

    Aula 08 Filtrando uma tabela com Regex

--- JAVASCRIPT-PROGRAMANDO-NA-LINGUAGEM-WEB/js/filtra.js
+++ JAVASCRIPT-PROGRAMANDO-NA-LINGUAGEM-WEB/js/filtra.js
@@ -1,24 +1,66 @@
 var campoFiltro = document.querySelector("#filtrar-tabela");
 
-campoFiltro.addEventListener("input", function(){
+campoFiltro.addEventListener("input", function() {
     var pacientes = document.querySelectorAll(".paciente");
 
-    if(this.value.length > 0){
-        for(var i = 0; i < pacientes.length; i++){
+    if (this.value.length > 0) {
+        for (var i = 0; i < pacientes.length; i++) {
             var paciente = pacientes[i];
             var tdNome = paciente.querySelector(".info-nome");
             var nome = tdNome.textContent;
+            var expressao = new RegExp(this.value, "i");
 
-            if(nome != this.value){
+            if (!expressao.test(nome)) {
                 paciente.classList.add("invisivel");
-            }else{
+            } else {
                 paciente.classList.remove("invisivel");
             }
         }
-    }else{
-        for(var i = 0; i < pacientes.length; i++){
+    } else {
+        for (var i = 0; i < pacientes.length; i++) {
             var paciente = pacientes[i];
             paciente.classList.remove("invisivel");
         }
     }
 });
+
+/*
+    Aula 08 Filtrando uma tabela com Regex
+
+    var expressao = new RegExp(this.value, "i"); 
+    //busca o conteudo de texto digitado no campo this.value, primeiro parâmetro é  padrão, 
+    (o texto da expressão regular, o que deve ser buscado) e o segundo parâmetro são uma ou mais 
+    flags (representando como queremos que a expressão regular busque)
+
+    O modificar "i" é para indicar que estamos buscando por "case-insensitive"
+
+    OUTRA FORMA DE FAZER A COMPARAÇÃO "SEM UTILIAR EXPERSSÕES REGULARES"
+
+    Usando a função substr, que recebe dois parâmetros, fazedo com que ela devolva parte da string,
+    com o tamanho definido nos parâmetros.
+
+    Exemplo: var string = "Alura";
+             var resultado = strin.substr(1,4);//resultado = lur
+
+
+    var comparavel = nome.substr(0, this.value.length);
+    if (!(this.value == comparavel)) {
+        paciente.classList.add("invisivel");
+    } else{
+        paciente.classList.remove("invisivel");
+    }
+
+    
+    //distinção entre letras maiúsculas e minúsculas
+    
+    var comparavel = nome.substr(0, this.value.length);
+    var comparavelMinusculo = comparavel.toLowerCase();
+    var valorDigitadoMinusculo = this.value.toLowerCase();
+    
+    if (!(valorDigitadoMinusculo == comparavelMinusculo)) {
+        paciente.classList.add("invisivel");
+    } else{
+        paciente.classList.remove("invisivel");
+    }
+
+*/
--- JAVASCRIPT-PROGRAMANDO-NA-LINGUAGEM-WEB/js/form.js
+++ JAVASCRIPT-PROGRAMANDO-NA-LINGUAGEM-WEB/js/form.js
@@ -12,110 +12,112 @@
 //target - alvo que foi clicado
 //parentNode - seleciona o pai do alvo clicado
 //setTimeOut - aguardar a execução de uma função por um tempo determinado
+//test() - testa oque o usuário está digitando.
+//substr() - recebe dois parâmetros, fazedo com que ela devolva parte da string, com o tamanho definido nos parâmetros.
 
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
-	botaoAdicionar.addEventListener("click", function(event){
-		event.preventDefault();
+botaoAdicionar.addEventListener("click", function(event) {
+    event.preventDefault();
 
-		var form = document.querySelector("#form-adiciona");
-		//Extraindo informações do paciente do form
-		var paciente = obtemPacienteDoFormulario(form);
-		
-		var pacienteTr = montatr(paciente);
+    var form = document.querySelector("#form-adiciona");
+    //Extraindo informações do paciente do form
+    var paciente = obtemPacienteDoFormulario(form);
 
-		var erros = validaPaciente(paciente);
+    var pacienteTr = montatr(paciente);
 
-		if(erros.length > 0){
-			exibeMensagensDeErro(erros);
-			return;
-		}
+    var erros = validaPaciente(paciente);
 
-		//Colocando as informações na tabela, adicionando o paciente na tabela
-		var tabela = document.querySelector("#tabela-pacientes");
-		tabela.appendChild(pacienteTr);
-		
-		form.reset();
-		var mensagensErro = document.querySelector("#mensagens-erro");
-		mensagensErro.innerHTML = "Cadastrado com Sucesso";//Limpa as mensagem de erro e adiciona a mensagem Cadastrado com Sucesso
-	});
+    if (erros.length > 0) {
+        exibeMensagensDeErro(erros);
+        return;
+    }
 
-	function exibeMensagensDeErro(erros){
-		var ul = document.querySelector("#mensagens-erro");
-		ul.innerHTML = "";//apaga as mensagens de erro insere as novas, conforme o usuário preenche o formulário
-		erros.forEach(function(erro) {
-			var li = document.createElement("li");
-			li.textContent = erro;
-			ul.appendChild(li);
-		});
-	}
+    //Colocando as informações na tabela, adicionando o paciente na tabela
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
 
-	function obtemPacienteDoFormulario(form){
+    form.reset();
+    var mensagensErro = document.querySelector("#mensagens-erro");
+    mensagensErro.innerHTML = "Cadastrado com Sucesso"; //Limpa as mensagem de erro e adiciona a mensagem Cadastrado com Sucesso
+});
 
-		var paciente = {
-			nome: form.nome.value,
-			peso: form.peso.value,
-			altura: form.altura.value,
-			gordura: form.gordura.value,
-			imc: calculaImc(form.peso.value, form.altura.value)
-		}
+function exibeMensagensDeErro(erros) {
+    var ul = document.querySelector("#mensagens-erro");
+    ul.innerHTML = ""; //apaga as mensagens de erro insere as novas, conforme o usuário preenche o formulário
+    erros.forEach(function(erro) {
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li);
+    });
+}
 
-		return paciente;	
-		
-	}
+function obtemPacienteDoFormulario(form) {
 
-	function montatr(paciente){
-		//Criando a nova linha, tr e td do paciente
-		var pacienteTr  = document.createElement("tr");
-		pacienteTr.classList.add("paciente");	
-	
-		//Adicionando elemento dentro do outro
-		pacienteTr .appendChild(montaTd(paciente.nome, "info-nome"));
-		pacienteTr .appendChild(montaTd(paciente.peso, "info-peso"));
-		pacienteTr .appendChild(montaTd(paciente.altura, "info-altura"));
-        pacienteTr .appendChild(montaTd(paciente.gordura, "info-gordura"));
-		pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
-		
-		return pacienteTr;
-	}
+    var paciente = {
+        nome: form.nome.value,
+        peso: form.peso.value,
+        altura: form.altura.value,
+        gordura: form.gordura.value,
+        imc: calculaImc(form.peso.value, form.altura.value)
+    }
 
-	function montaTd(dado,classe){
-		var td = document.createElement("td");
-		td.textContent = dado;
-		td.classList.add(classe);
+    return paciente;
 
-		return td;
-	}
+}
 
-	/**
-	 Aula 06 Validação de formularios
-	 */
+function montatr(paciente) {
+    //Criando a nova linha, tr e td do paciente
+    var pacienteTr = document.createElement("tr");
+    pacienteTr.classList.add("paciente");
 
-	 function validaPaciente(paciente) {
+    //Adicionando elemento dentro do outro
+    pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
+    pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"));
+    pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"));
+    pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"));
+    pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
 
-		var erros = [];
+    return pacienteTr;
+}
 
-		if(paciente.nome.length == 0){
-			erros.push("O nome não pode ser em branco");
-		}
+function montaTd(dado, classe) {
+    var td = document.createElement("td");
+    td.textContent = dado;
+    td.classList.add(classe);
 
-		 if(!validaPeso(paciente.peso)){
-			  erros.push("Peso é inválido");
-		 }
-		 if(!validaAltura(paciente.altura)){		 
-			 erros.push("Altura é inválida!");
-		 }
+    return td;
+}
 
-		 if(paciente.gordura.length == 0){
-			 erros.push("A gordura não pode ser em branco");
-		 }
+/**
+ Aula 06 Validação de formularios
+ */
 
-		 if(paciente.peso.length == 0){
-			 erros.push("O peso não pode ser em branco");
-		 }
+function validaPaciente(paciente) {
 
-		 if(paciente.altura.length == 0){
-			 erros.push("A altura não pode ser em branco");
-		 }
+    var erros = [];
 
-		 return erros;
-	 }
+    if (paciente.nome.length == 0) {
+        erros.push("O nome não pode ser em branco");
+    }
+
+    if (!validaPeso(paciente.peso)) {
+        erros.push("Peso é inválido");
+    }
+    if (!validaAltura(paciente.altura)) {
+        erros.push("Altura é inválida!");
+    }
+
+    if (paciente.gordura.length == 0) {
+        erros.push("A gordura não pode ser em branco");
+    }
+
+    if (paciente.peso.length == 0) {
+        erros.push("O peso não pode ser em branco");
+    }
+
+    if (paciente.altura.length == 0) {
+        erros.push("A altura não pode ser em branco");
+    }
+
+    return erros;
+}
