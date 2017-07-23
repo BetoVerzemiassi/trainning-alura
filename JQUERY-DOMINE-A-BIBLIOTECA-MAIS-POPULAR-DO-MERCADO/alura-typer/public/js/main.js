f3c1956ba93201e58ee7d213c6065a3b892bfe1e
BetoVerzemiassi <betoverzemiassi@gmail.com>
2017-07-23 20:42:04 -0300

    Organizando arquivo main.js

--- JQUERY-DOMINE-A-BIBLIOTECA-MAIS-POPULAR-DO-MERCADO/alura-typer/public/js/main.js
+++ JQUERY-DOMINE-A-BIBLIOTECA-MAIS-POPULAR-DO-MERCADO/alura-typer/public/js/main.js
@@ -1,27 +1,8 @@
-/**
- Aula 01 Introdução ao jQuery
-
- text() - acessa o conteudo de texto, retorna em formato de string
- split() - quebra uma string em espaços
- length - retorna o tamanho
- */
-
 var frase = $(".frase").text();
 var numeroPalavras = frase.split(" ").length;
 var tamanhoFrase = $("#tamanho-frase");//text(); sem argumento ela pega o conteudo
 
 tamanhoFrase.text(numeroPalavras);//com argumento ela coloca o conteudo
-
-
-
- /**
-  Aula 02 Evento com jQuery
-
-  on() - faz a ação do evento
-  val() - pega o valor digitado no campo textarea, esse metodo da acesso ao que está dentro de uma tag de input, 
-  como as tags input e textarea; já o text nos dá acesso ao que está dentro de uma tag de texto, como p, span e h1
-  
-  */
 
   var campo = $(".campo-digitacao");
   campo.on("input", function(){
@@ -33,17 +14,6 @@ tamanhoFrase.text(numeroPalavras);//com argumento ela coloca o conteudo
       var qtdCaracteres = conteudo.length;
       $(".contador-caracteres").text(qtdCaracteres);
   });
-
-
-  /**
-   Aula 03 GameOver com eventos
-
-   focus() - o campo ganha o foco para ser digitado.
-   setInterval() - faz com que uma determinada ação seja executada em um intervalo de tempo.
-   attr() - adiciona um atributo
-   clearInterval() - recebe o id do setInterval como parâmetro e para com a contagem
-   one() - essa função escuta o evento apenas uma vez ao contrário do "on() - onde fica o tempo todo escutando o evento"
-   */
 
    var tempoRestante = $("#tempo-digitacao").text();
    campo.one("focus", function(){
@@ -57,3 +27,30 @@ tamanhoFrase.text(numeroPalavras);//com argumento ela coloca o conteudo
             }            
         },1000);
    });
+
+
+   /**
+     Aula 01 Introdução ao jQuery
+
+    text() - acessa o conteudo de texto, retorna em formato de string
+    split() - quebra uma string em espaços
+    length - retorna o tamanho
+    */
+
+    /**
+    Aula 02 Evento com jQuery
+
+    on() - faz a ação do evento
+    val() - pega o valor digitado no campo textarea, esse metodo da acesso ao que está dentro de uma tag de input, 
+    como as tags input e textarea; já o text nos dá acesso ao que está dentro de uma tag de texto, como p, span e h1  
+    */
+
+   /**
+   Aula 03 GameOver com eventos
+
+   focus() - o campo ganha o foco para ser digitado.
+   setInterval() - faz com que uma determinada ação seja executada em um intervalo de tempo.
+   attr() - adiciona um atributo
+   clearInterval() - recebe o id do setInterval como parâmetro e para com a contagem
+   one() - essa função escuta o evento apenas uma vez ao contrário do "on() - onde fica o tempo todo escutando o evento"
+   */
