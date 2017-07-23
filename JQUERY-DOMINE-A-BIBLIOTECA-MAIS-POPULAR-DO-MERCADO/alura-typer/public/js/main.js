d47fd407c33d7358606d9609dcda915a02dad04a
BetoVerzemiassi <betoverzemiassi@gmail.com>
2017-07-23 15:50:25 -0300

    Aula 02 Escutando Eventos com jQuery

--- JQUERY-DOMINE-A-BIBLIOTECA-MAIS-POPULAR-DO-MERCADO/alura-typer/public/js/main.js
+++ JQUERY-DOMINE-A-BIBLIOTECA-MAIS-POPULAR-DO-MERCADO/alura-typer/public/js/main.js
@@ -1,9 +1,3 @@
-var frase = $(".frase").text();
-var numeroPalavras = frase.split(" ").length;
-var tamanhoFrase = $("#tamanho-frase");//text(); sem argumento ela pega o conteudo
-
-tamanhoFrase.text(numeroPalavras);//com argumento ela coloca o conteudo
-
 /**
  Aula 01 Introdução ao jQuery
 
@@ -11,3 +5,31 @@ tamanhoFrase.text(numeroPalavras);//com argumento ela coloca o conteudo
  split() - quebra uma string em espaços
  length - retorna o tamanho
  */
+
+var frase = $(".frase").text();
+var numeroPalavras = frase.split(" ").length;
+var tamanhoFrase = $("#tamanho-frase");//text(); sem argumento ela pega o conteudo
+
+tamanhoFrase.text(numeroPalavras);//com argumento ela coloca o conteudo
+
+
+
+ /**
+  *Aula 02 Evento com jQuery
+
+  on() - faz a ação do evento
+  val() - pega o valor digitado no campo textarea, esse metodo da acesso ao que está dentro de uma tag de input, 
+  como as tags input e textarea; já o text nos dá acesso ao que está dentro de uma tag de texto, como p, span e h1
+  
+  */
+
+  var campo = $(".campo-digitacao");
+  campo.on("input", function(){
+      var conteudo = campo.val();
+
+      var qtdPlavras = conteudo.split(/\S+/).length - 1;
+      $(".contador-palavras").text(qtdPlavras);
+
+      var qtdCaracteres = conteudo.length;
+      $(".contador-caracteres").text(qtdCaracteres);
+  });
