2e02a222ec53a8113ff93a74941d6119540fd82b
alberto.borguesani <alberto.borguesani@infracommerce.com.br>
2017-07-26 15:14:51 -0300

    Aula 04 Reiniciando o jogo

--- JQUERY-DOMINE-A-BIBLIOTECA-MAIS-POPULAR-DO-MERCADO/alura-typer/public/js/main.js
+++ JQUERY-DOMINE-A-BIBLIOTECA-MAIS-POPULAR-DO-MERCADO/alura-typer/public/js/main.js
@@ -1,56 +1,96 @@
-var frase = $(".frase").text();
-var numeroPalavras = frase.split(" ").length;
-var tamanhoFrase = $("#tamanho-frase");//text(); sem argumento ela pega o conteudo
+var tempoInicial = $("#tempo-digitacao").text();
+var campo = $(".campo-digitacao");
 
-tamanhoFrase.text(numeroPalavras);//com argumento ela coloca o conteudo
+$(function() {
+    atualizaTamanhoFrase();
+    inicializaContadores();
+    inicializaCronometro();
+    $("#botao-reiniciar").click(reiniciaJogo);
+});
 
-  var campo = $(".campo-digitacao");
-  campo.on("input", function(){
-      var conteudo = campo.val();
+function atualizaTamanhoFrase() {
+    var frase = $(".frase").text();
+    var numeroPalavras = frase.split(" ").length;
+    var tamanhoFrase = $("#tamanho-frase"); //text(); sem argumento ela pega o conteudo
+    tamanhoFrase.text(numeroPalavras); //com argumento ela coloca o conteudo
+}
 
-      var qtdPlavras = conteudo.split(/\S+/).length - 1;
-      $(".contador-palavras").text(qtdPlavras);
+function inicializaContadores() {
+    campo.on("input", function() {
+        var conteudo = campo.val();
 
-      var qtdCaracteres = conteudo.length;
-      $(".contador-caracteres").text(qtdCaracteres);
-  });
+        var qtdPlavras = conteudo.split(/\S+/).length - 1;
+        $(".contador-palavras").text(qtdPlavras);
 
-   var tempoRestante = $("#tempo-digitacao").text();
-   campo.one("focus", function(){
-        var cronometroID = setInterval(function(){
+        var qtdCaracteres = conteudo.length;
+        $(".contador-caracteres").text(qtdCaracteres);
+    });
+}
+
+function inicializaCronometro() {
+    var tempoRestante = $("#tempo-digitacao").text();
+    campo.one("focus", function() {
+        $("#botao-reiniciar").attr("disabled", true);
+        var cronometroID = setInterval(function() {
             tempoRestante--;
-            console.log(tempoRestante);
             $("#tempo-digitacao").text(tempoRestante);
-            if(tempoRestante < 1){
-                campo.attr("disabled",true);
-                clearInterval(cronometroID);//para com a contagem assim que acabar o tempo
-            }            
-        },1000);
-   });
+            if (tempoRestante < 1) {
+                campo.attr("disabled", true);
+                clearInterval(cronometroID); //para com a contagem assim que acabar o tempo
+                $("#botao-reiniciar").attr("disabled", false);
+            }
+        }, 1000);
+    });
+}
+
+function reiniciaJogo() {
+    campo.attr("disabled", false);
+    campo.val("");
+    $("#contador-palavras").text("0");
+    $("#contador-caracteres").text("0");
+    $("#tempo-digitacao").text(tempoInicial);
+    inicializaCronometro(); //chamamos a função;
+}
 
 
-   /**
-     Aula 01 Introdução ao jQuery
 
-    text() - acessa o conteudo de texto, retorna em formato de string
-    split() - quebra uma string em espaços
-    length - retorna o tamanho
-    */
+/**
+  Aula 01 Introdução ao jQuery
 
-    /**
-    Aula 02 Evento com jQuery
+ text() - acessa o conteudo de texto, retorna em formato de string
+ split() - quebra uma string em espaços
+ length - retorna o tamanho
+ */
 
-    on() - faz a ação do evento
-    val() - pega o valor digitado no campo textarea, esse metodo da acesso ao que está dentro de uma tag de input, 
-    como as tags input e textarea; já o text nos dá acesso ao que está dentro de uma tag de texto, como p, span e h1  
-    */
+/**
+Aula 02 Evento com jQuery
 
-   /**
-   Aula 03 GameOver com eventos
+on() - faz a ação do evento
+val() - pega o valor digitado no campo textarea, esse metodo da acesso ao que está dentro de uma tag de input, 
+como as tags input e textarea; já o text nos dá acesso ao que está dentro de uma tag de texto, como p, span e h1  
+*/
 
-   focus() - o campo ganha o foco para ser digitado.
-   setInterval() - faz com que uma determinada ação seja executada em um intervalo de tempo.
-   attr() - adiciona um atributo
-   clearInterval() - recebe o id do setInterval como parâmetro e para com a contagem
-   one() - essa função escuta o evento apenas uma vez ao contrário do "on() - onde fica o tempo todo escutando o evento"
-   */
+/**
+Aula 03 GameOver com eventos
+
+focus() - o campo ganha o foco para ser digitado.
+setInterval() - faz com que uma determinada ação seja executada em um intervalo de tempo.
+attr() - adiciona um atributo
+clearInterval() - recebe o id do setInterval como parâmetro e para com a contagem
+one() - essa função escuta o evento apenas uma vez ao contrário do "on() - onde fica o tempo todo escutando o evento"
+*/
+
+/**
+ Aula 04 Reiniciando o jogo
+
+ Podemos executar o evento de click com a função de atalho click();
+
+ $(document).ready() - Aguarda a página ser carregada e depois executa seu conteúdo. Maneira mais comum de escrever
+ $(function() { ... });
+ click() - função de atalho .on("click"), tem o mesmo comportamento
+ removeAttr() - objetivo remover atributos de elementos. Exemplo abaixo
+
+ Ex: $(".post").removeAttr("disabled");
+
+ 
+ */
