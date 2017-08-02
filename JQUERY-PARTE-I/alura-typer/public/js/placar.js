f80da19bdafcdcfa335a743449df5eac497481f8
alberto.borguesani <alberto.borguesani@infracommerce.com.br>
2017-08-02 16:25:00 -0300

    Ajuste no projeto Alura Typer - Conflitos nos arquivos

--- JQUERY-PARTE-I/alura-typer/public/js/placar.js
+++ JQUERY-PARTE-I/alura-typer/public/js/placar.js
@@ -1,35 +1,35 @@
-function inserePlacar(){
+function inserePlacar() {
     var corpoTabela = $(".placar").find("tbody");
-    var usuario = "Alberto";
-    var numPalavras = $("#contador-palavras").text(); 
+    var usuario = "Douglas"
+    var numPalavras = $("#contador-palavras").text();
 
     var linha = novaLinha(usuario, numPalavras);
     linha.find(".botao-remover").click(removeLinha);
 
-    corpoTabela.prepend(linha);
-}  
+    corpoTabela.append(linha);
+}
 
-function novaLinha(usuario,palavras){
-    var linha = $("<tr>");//Função que cria elementos no HTML
+function novaLinha(usuario, palavras) {
+    var linha = $("<tr>");
     var colunaUsuario = $("<td>").text(usuario);
-    var colunaPalavras = $("<td>").text(numPalavras);
+    var colunaPalavras = $("<td>").text(palavras);
     var colunaRemover = $("<td>");
 
-    var link = $("<a>").addClass("botao-remover").attr("href","#");
-    var icone = $("<i>").addClass("smal").addClass("material-icons").text("delete");
+    var link = $("<a>").addClass("botao-remover").attr("href", "#");
+    var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
 
     link.append(icone);
-    
+
     colunaRemover.append(link);
 
     linha.append(colunaUsuario);
     linha.append(colunaPalavras);
-    linha.append(colunaRemover);  
-    
+    linha.append(colunaRemover);
+
     return linha;
 }
 
-function removeLinha(){  
-    e.preventDefault();
-    $(this).parent().parent().remove();  
+function removeLinha() {
+    event.preventDefault();
+    $(this).parent().parent().remove();
 }
