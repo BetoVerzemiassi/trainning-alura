2990b50ea1976fe7d0ecc568181c333f85158a48
alberto.borguesani <alberto.borguesani@infracommerce.com.br>
2017-08-02 16:31:19 -0300

    Aula 01 Mostrando o Placar

--- JQUERY-PARTE-I/alura-typer/public/js/main.js
+++ JQUERY-PARTE-I/alura-typer/public/js/main.js
@@ -11,7 +11,7 @@ $(function() {
 
 function atualizaTamanhoFrase() {
     var frase = $(".frase").text();
-    var numPalavras  = frase.split(" ").length;
+    var numPalavras = frase.split(" ").length;
     var tamanhoFrase = $("#tamanho-frase");
 
     tamanhoFrase.text(numPalavras);
@@ -48,14 +48,14 @@ function inicializaMarcadores() {
 function inicializaCronometro() {
     var tempoRestante = $("#tempo-digitacao").text();
     campo.one("focus", function() {
-    	var cronometroID = setInterval(function() {
-    		tempoRestante--;
-    		$("#tempo-digitacao").text(tempoRestante);
-    		if (tempoRestante < 1) {
+        var cronometroID = setInterval(function() {
+            tempoRestante--;
+            $("#tempo-digitacao").text(tempoRestante);
+            if (tempoRestante < 1) {
                 clearInterval(cronometroID);
                 finalizaJogo();
-    		}
-    	}, 1000);
+            }
+        }, 1000);
     });
 }
 
@@ -76,3 +76,97 @@ function reiniciaJogo() {
     campo.removeClass("borda-vermelha");
     campo.removeClass("borda-verde");
 }
+
+/**
+  Aula 01 Introdução ao jQuery
+
+ text() - acessa o conteudo de texto, retorna em formato de string
+ split() - quebra uma string em espaços
+ length - retorna o tamanho
+ */
+
+/**
+Aula 02 Evento com jQuery
+
+on() - faz a ação do evento
+val() - pega o valor digitado no campo textarea, esse metodo da acesso ao que está dentro de uma tag de input, 
+como as tags input e textarea; já o text nos dá acesso ao que está dentro de uma tag de texto, como p, span e h1  
+*/
+
+/**
+Aula 03 GameOver com eventos
+
+focus() - o campo ganha o foco para ser digitado.
+setInterval() - faz com que uma determinada ação seja executada em um intervalo de tempo.
+attr() - adiciona um atributo
+clearInterval() - recebe o id do setInterval como parâmetro e para com a contagem
+one() - essa função escuta o evento apenas uma vez ao contrário do "on() - onde fica o tempo todo escutando o evento"
+*/
+
+/**
+ Aula 04 Reiniciando o jogo
+
+ Podemos executar o evento de click com a função de atalho click();
+
+ $(document).ready() - Aguarda a página ser carregada e depois executa seu conteúdo. Maneira mais comum de escrever
+ $(function() { ... });
+ click() - função de atalho .on("click"), tem o mesmo comportamento
+ removeAttr() - objetivo remover atributos de elementos. Exemplo abaixo
+
+ Ex: $(".post").removeAttr("disabled");
+
+ 
+ */
+
+/**
+  Aula 05 Funções que auxiliam os estilos
+
+  css() - Altera o estilo, passando a propriedade que queremos e seu valor. Porém sabemos que o certo não é certo
+  mexer com estilo de uma página diretamente no JavaScript. Logo devemos mexer em css. Com a função abaixo adicionamos
+  a classe com o estilo desejado.
+  addClass() - Adiciona uma classe ao campo selecionado da página.
+  removeClass() - Remove a classe do campo selecionado da página.
+  toggleClass() - Funciona da seguinte maneira, se no momento que a função for chamada, o elemento possuir a classe, 
+  ela será removida. Mas se o elemento não possuir a classe, ela será adicionada.
+
+    campo.toggleClass("campo-desativado", true); //sempre adiciona
+
+  substr() - Pega pedaço de uma string 
+
+================================================================  
+
+PARA SABER MAIS: SUBSTR COM ES6
+
+  Como o JavaScript está evoluindo e melhorando já existe uma forma mais fácil de verificar se uma string faz parte 
+  da outra string. Se o seu navegador já dar suporte ao ECMA Script 6 você pode simplesmente executar:
+
+ var digitouCorreto = frase.startsWith(digitado);
+    if(digitouCorreto) {
+        campo.addClass("borda-verde");
+    } else {
+        campo.addClass("borda-vermelha");
+    }
+Ainda mais enxuto:
+
+if( frase.startsWith(digitado)) {
+ campo.addClass("borda-verde");
+} else {
+ campo.addClass("borda-vermelha");
+}
+A função startsWith devolve true ou false, se a frase começa com o valor digitado ou não. Teste isso agora no console, 
+digitando por exemplo:
+
+"ECMA Script 6".startsWith("ECMA")
+  */
+
+/**
+   Aula 06 Criando e Manipulando elementos com jQuery
+
+   find() - Busca filho de um elemento que já selecionamos previamente, podemos utilizar a função .find() do 
+   jQuery, que funciona de modo semelhante a função seletora ($), porém fazendo a busca apenas do filho do elemento.
+   append() - Adiciona a string ou elemento HTML que  é passada como parâmetro com último filho do elemento em qual
+   ela for chamada.
+   prepend() - Adiciona a string/html passada como primeiro filho do elemento selecionado.
+   remove() - Remove o item selecionado.
+   parent() - Acessa o elemento acima do elemento selecionado com jQuery
+ */
--- JQUERY-PARTE-I/alura-typer/public/js/placar.js
+++ JQUERY-PARTE-I/alura-typer/public/js/placar.js
@@ -1,6 +1,6 @@
 function inserePlacar() {
     var corpoTabela = $(".placar").find("tbody");
-    var usuario = "Douglas"
+    var usuario = "Alberto"
     var numPalavras = $("#contador-palavras").text();
 
     var linha = novaLinha(usuario, numPalavras);
@@ -33,3 +33,23 @@ function removeLinha() {
     event.preventDefault();
     $(this).parent().parent().remove();
 }
+
+//jQuery parte II
+
+$("#botao-placar").click(mostraPlacar);
+
+function mostraPlacar() {
+    $(".placar").slideToggle(600);
+}
+
+/*
+Curso Jquery Parte II
+
+Aula 01 Animações com jQuery
+
+show() - Mostra o conteúdo
+hide() - Esconde o conteúdo
+toggle() - Adiciona ou esconde um elemento
+slideDown() - Realiza a transição de uma maneira mais suave, podemos passar por parâmetro para a função .slideDown(600);
+slideToggle() - Essa função alterna entre as funções slideDown() e slideUp()
+*/
