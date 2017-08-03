fee98b6ef208d1851c07a58f6036b30e2830a081
alberto.borguesani <alberto.borguesani@infracommerce.com.br>
2017-08-03 13:57:45 -0300

    Aula 01 Finalizando Animacoes com jQuery

--- JQUERY-PARTE-I/alura-typer/public/js/placar.js
+++ JQUERY-PARTE-I/alura-typer/public/js/placar.js
@@ -7,6 +7,17 @@ function inserePlacar() {
     linha.find(".botao-remover").click(removeLinha);
 
     corpoTabela.append(linha);
+
+    $(".placar").slideDown(500);
+    scrollPlacar();
+}
+
+function scrollPlacar() {
+    var posicaoPlacar = $(".placar").offset().top();
+
+    $("body").animate({
+        scrollTop: posicaoPlacar + "px"
+    }, 1000);
 }
 
 function novaLinha(usuario, palavras) {
@@ -44,7 +55,7 @@ function removeLinha() {
 $("#botao-placar").click(mostraPlacar);
 
 function mostraPlacar() {
-    $(".placar").slideToggle(600);
+    $(".placar").stop().slideToggle(600);
 }
 
 /*
@@ -58,6 +69,36 @@ toggle() - Adiciona ou esconde um elemento
 slideDown() - Realiza a transição de uma maneira mais suave, podemos passar por parâmetro para a função .slideDown(600);
 slideToggle() - Essa função alterna entre as funções slideDown() e slideUp()
 fadeOut() - Diminui a opacidade de um elemento aos poucos, até o seu total desaparecimento.
+stop() - A animação que estiver acontecendo no momento é interrompida, e uma próxima é iniciada.
+
+offset() - Ela nos dá a posição em que determinado elemento se encontra na página. Retorna a distância em que o elemento está
+do topo e da esquerda da página. Se acessarmos o valor do topo (top), teremos o valor exato para onde queremos scrollar a página.
+
+animate() - Função responsável por animação, recebe dois parâmetros, um objeto que contém as propriedades CSS a serem animadas
+e os seus valores, e o TEMPO DE DURAÇÃO da animação.
+
+scrollTop - Realiza o scroll, recebe um valor em pixels, que representa a posição da nossa página para onde desejamos scrollar a 
+página.
+
+hasClass() - Retorna true se um elemento possui ou não uma classe.
+
+OBS: O jQuery possui a função is que permite consultar uma pseudo class. Toda vez que um elemento está com display diferente de none ele
+ganha a pseudo classe :visible. A função is retorna true caso o elemento esteja visível. Se ele estiver visível, precisamos escondê-lo
+e isso é feito através da função hide(). Para exibir o elemento, é usada a função show().
+
+Exemplo: 
+
+$('#botao-promocao').click(function() {
+
+  var promocoes = $('.promocoes');
+  if(promocoes.is(':visible')) {
+
+    promocoes.hide();
+  } else {
+    promocoes.show();
+  }
+
+});
 
 Então temos sempre que ter em mente que devemos remover um elemento após realizar o fadeOut(), já que essa função só faz
 com que o elemento desaparecça da tela, mas ainda fique no html da página.
